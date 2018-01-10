3c00ba1c333c027c72e9a87befe39e85cdf9e8ce
frohenk <frohenk@gmail.com>
2018-01-10 10:01:12 +0300

    Deploy

--- src/main/java/mem/sirius/example/java/App.java
+++ src/main/java/mem/sirius/example/java/App.java
@@ -6,12 +6,13 @@ import java.net.Socket;
 
 // Zdes bil ya
 public class App {
+    public static final String PORT = "PORT";
     public static int port = 0;
     public static final String myip = "http://memsproblems.000webhostapp.com/ac/";
     public static final String mem_dir = "/home/azat/memes";
 
     public static void main(String[] args) {
-        port = Integer.parseInt(System.getenv("PORT") != null ? System.getenv("$PORT") : "5000");
+        port = Integer.parseInt(System.getenv(PORT) != null ? System.getenv(PORT) : "5000");
         System.out.println("Port is :" + port);
         ServerSocket ss = null;
         try {
