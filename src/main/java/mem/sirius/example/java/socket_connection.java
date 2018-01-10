dd778d75085e156176055cd36d728eb74e31e2cf
frohenk <frohenk@gmail.com>
2018-01-10 09:50:03 +0300

    Deploy

--- src/main/java/mem/sirius/example/java/socket_connection.java
+++ src/main/java/mem/sirius/example/java/socket_connection.java
@@ -1,16 +1,15 @@
 package mem.sirius.example.java;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 
 
 public class socket_connection extends Thread {
     private Socket socket;
 
-    public socket_connection (Socket ss) {
+    public socket_connection(Socket ss) {
         socket = ss;
     }
 
@@ -20,36 +19,52 @@ public class socket_connection extends Thread {
         try {
             InputStream sin = socket.getInputStream();
             OutputStream sout = socket.getOutputStream();
-
-            DataInputStream in = new DataInputStream(sin);
-            DataOutputStream out = new DataOutputStream(sout);
-
-            String line = null;
+            ArrayList<Integer> data = new ArrayList<Integer>();
             while (true) {
-                try {
-                    line = in.readUTF();
-                } catch (Exception x) {
-                    socket.close();
+                data.add(sin.read());
+                if (data.size() >= 4 && data.get(data.size() - 1) == '\n' && data.get(data.size() - 2) == '\r' && data.get(data.size() - 3) == '\n' && data.get(data.size() - 4) == '\r')
                     break;
-                }
-                Request request = new Request(line);
-                if (request.links.get("type").equals("get_list")){
-                    get_list response = new get_list(request);
-                    String ans = response.getResponse().toString();
-                    if (ans != null) {
-                        System.out.println("New answer for getlist " + ans);
-                        System.out.println();
-                        out.writeUTF(ans);
-                        out.flush();
-                    }
-                    continue;
-                }
-                if (request.links.get("type").equals("close")) {
-                    System.out.println("User decide to close");
-                    socket.close();
-                    break;
-                }
             }
+
+            String defaultResponse = "HTTP/1.1 200 OK\r\n" +
+                    "Server: Meme\r\n" +
+                    "X-Powered-By: Some guys laptop\r\n" +
+                    "Content-Language: ru\r\n" +
+                    "Content-Type: text/html; charset=utf-8\r\n\r\n";
+            sout.write(defaultResponse.getBytes());
+
+            sout.write("kek".getBytes());
+            sout.close();
+
+//            DataInputStream in = new DataInputStream(sin);
+//            DataOutputStream out = new DataOutputStream(sout);
+//
+//            String line = null;
+//            while (true) {
+//                try {
+//                    line = in.readUTF();
+//                } catch (Exception x) {
+//                    socket.close();
+//                    break;
+//                }
+//                Request request = new Request(line);
+//                if (request.links.get("type").equals("get_list")) {
+//                    get_list response = new get_list(request);
+//                    String ans = response.getResponse().toString();
+//                    if (ans != null) {
+//                        System.out.println("New answer for getlist " + ans);
+//                        System.out.println();
+//                        out.writeUTF(ans);
+//                        out.flush();
+//                    }
+//                    continue;
+//                }
+//                if (request.links.get("type").equals("close")) {
+//                    System.out.println("User decide to close");
+//                    socket.close();
+//                    break;
+//                }
+//            }
         } catch (Exception x) {
             x.printStackTrace();
         }
