b146107c5d325e7a0998ca1962d58e5a4236b2bb
frohenk <frohenk@gmail.com>
2018-01-12 13:35:41 +0300

    KEK

--- src/main/java/mem/sirius/example/java/Response.java
+++ src/main/java/mem/sirius/example/java/Response.java
@@ -3,13 +3,10 @@ package mem.sirius.example.java;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.util.ArrayList;
-import java.util.TreeMap;
-
 public class Response {
-    private TreeMap<String, ArrayList<String>> links = new TreeMap<String, ArrayList<String>>();
+    private Object links;
 
-    public Response(TreeMap<String, ArrayList<String>> a) {
+    public Response(Object a) {
         links = a;
     }
     public String toString(){
